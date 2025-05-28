c77807c6b1e9cf52320599f348c1c4c0b1f75d13
Sarath <sarathkrishna357@gmail.com>
2025-05-28 20:09:16 +0530

    Updated todo-cli

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-env jest */
+
+/*
 const todoList = require("../todo");
 
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
@@ -70,4 +72,29 @@ describe("Todo test suite", () => {
         expect(dueLaterItems.length).toBe(1);
         expect(dueLaterItems[0].title).toBe("Due Later Todo");
     });
+});
+
+*/
+
+
+
+// __tests__/todo.js
+/* eslint-disable no-undef */
+const db = require("../models");
+
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date(),
+    });
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
+  });
 });
